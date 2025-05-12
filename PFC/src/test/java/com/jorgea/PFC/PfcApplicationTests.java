e898682523030428d7c1a81777c54ccd74a48ecc
jorge.canosa <jorge220604@gmail.com>
2025-05-12 08:28:55 +0200

    Add files via upload

---
+++ PFC/src/test/java/com/jorgea/PFC/PfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgea.PFC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
