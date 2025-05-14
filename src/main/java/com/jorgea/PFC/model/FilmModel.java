7082093ae9f10560c6adcdff0e644948aca97e50
jorge.alvarez <jorge220604@gmail.com>
2025-05-14 08:21:09 +0200

    creación de setters, getters y constructores

--- src/main/java/com/jorgea/PFC/model/FilmModel.java
+++ src/main/java/com/jorgea/PFC/model/FilmModel.java
@@ -14,4 +14,61 @@ public class FilmModel {
     private String director;
     private String releaseDate;
     private double rating;
+
+    public FilmModel(int filmId, String title, String description, String director, String releaseDate, double rating) {
+        this.filmId = filmId;
+        this.title = title;
+        this.description = description;
+        this.director = director;
+        this.releaseDate = releaseDate;
+        this.rating = rating;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
 }
--- src/main/java/com/jorgea/PFC/model/SerieModel.java
+++ src/main/java/com/jorgea/PFC/model/SerieModel.java
@@ -14,4 +14,61 @@ public class SerieModel {
     private String director;
     private String releaseDate;
     private double rating;
+
+    public SerieModel(int filmId, String title, String description, String director, String releaseDate, double rating) {
+        this.filmId = filmId;
+        this.title = title;
+        this.description = description;
+        this.director = director;
+        this.releaseDate = releaseDate;
+        this.rating = rating;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
