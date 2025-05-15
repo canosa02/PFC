d4b71050a03d74a610919f129826bbad56e8c8f4
jorge.alvarez <jorge220604@gmail.com>
2025-05-15 20:59:02 +0200

    corrección de gameId null y arreglo creación automática de game_genre

---
+++ target/generated-sources/annotations/com/jorgea/PFC/mapperDto/GenresDtoMapperImpl.java
@@ -0,0 +1,54 @@
+package com.jorgea.PFC.mapperDto;
+
+import com.jorgea.PFC.dto.GamesDto;
+import com.jorgea.PFC.dto.GenresDto;
+import com.jorgea.PFC.to.GenresTo;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-15T20:57:26+0200",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
+)
+@Component
+public class GenresDtoMapperImpl implements GenresDtoMapper {
+
+    @Override
+    public GenresTo toGenresTo(GenresDto genresDto) {
+        if ( genresDto == null ) {
+            return null;
+        }
+
+        GenresTo genresTo = new GenresTo();
+
+        genresTo.setGenresId( genresDto.getGenresId() );
+        genresTo.setGenreName( genresDto.getGenreName() );
+        List<GamesDto> list = genresDto.getGames();
+        if ( list != null ) {
+            genresTo.setGames( new ArrayList<GamesDto>( list ) );
+        }
+
+        return genresTo;
+    }
+
+    @Override
+    public GenresDto toGenresDto(GenresTo genresTo) {
+        if ( genresTo == null ) {
+            return null;
+        }
+
+        GenresDto genresDto = new GenresDto();
+
+        genresDto.setGenresId( genresTo.getGenresId() );
+        genresDto.setGenreName( genresTo.getGenreName() );
+        List<GamesDto> list = genresTo.getGames();
+        if ( list != null ) {
+            genresDto.setGames( new ArrayList<GamesDto>( list ) );
+        }
+
+        return genresDto;
+    }
+}
---
+++ target/generated-sources/annotations/com/jorgea/PFC/mapperModel/GenresModelMapperImpl.java
@@ -0,0 +1,11 @@
+package com.jorgea.PFC.mapperModel;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-15T20:57:27+0200",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
+)
+public class GenresModelMapperImpl implements GenresModelMapper {
+}
